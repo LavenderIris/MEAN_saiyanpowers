3ed5737ef06bacf638d782d8885a17c6cece98df
LavenderIris <pchung@gmail.com>
2017-11-04 17:29:36 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  powerlevel: Number = 0;
+
+  setPower() {
+    this.powerlevel = this.powerlevel;
+    console.log(this.powerlevel);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
 import { AppComponent } from './app.component';
+import { HumanComponent } from './human/human.component';
+import { SaiyanComponent } from './saiyan/saiyan.component';
+import { SupersaiyanComponent } from './supersaiyan/supersaiyan.component';
+import { SupersaiyantwoComponent } from './supersaiyantwo/supersaiyantwo.component';
+import { SupersaiyanthreeComponent } from './supersaiyanthree/supersaiyanthree.component';
+import { SupersaiyanfourComponent } from './supersaiyanfour/supersaiyanfour.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HumanComponent,
+    SaiyanComponent,
+    SupersaiyanComponent,
+    SupersaiyantwoComponent,
+    SupersaiyanthreeComponent,
+    SupersaiyanfourComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
